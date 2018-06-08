2b19f8a5d6bb428242bebb649dc5c47374330b7c
Luc Müller <lm@ityou.de>
2018-06-08 14:07:23 +0200

    support e-mail-adresse hinzu gefügt

--- simple_backend_theme/__manifest__.py
+++ simple_backend_theme/__manifest__.py
@@ -4,6 +4,7 @@
     "description": "Lightweight backend theme for Odoo 10/11 community edition.",
     "version":"1.0.2",
     "author":"ITYOU/LM",
+    "support":"LM@ityou.de",
     "website": "https://www.ityou.de",
     "license": "LGPL-3",
     "installable": True,
